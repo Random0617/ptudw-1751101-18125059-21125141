fabad2bf7a6bb3c5350eeba6d610533ea4e58b43
Tran Thuc Minh Tri <minhtri170603@gmail.com>
2024-06-10 08:26:55 +0700

    8:30 in part 4 video
    Bug: Header of home page does not work correctly (height is too large)

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const express = require("express");
 const app = express();
 //const port = express.env.PORT || 5000;
 const port = 5000;
+const expressHandlebars = require("express-handlebars");
 
 // Public static folder
 app.use(express.static(__dirname + "/public"));
 
+// Express-handlebars
+app.engine(
+  "hbs",
+  expressHandlebars.engine({
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+    extname: "hbs",
+    defaultLayout: "layout",
+  })
+);
+app.set("view engine", "hbs");
+// routes
 app.get("/", (req, res) => {
-  res.send("Hello to Eshop");
+  res.render("index");
 });
 
 // Starting the web server
